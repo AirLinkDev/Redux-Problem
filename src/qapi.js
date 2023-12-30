9d0dd9c84e84e80b4e4daff0936cc2dab889681d
AirLinkDev <91385331+AirLinkDev@users.noreply.github.com>
2023-12-30 07:42:18 -0800

    Changes Added babel-plugin-transform-class-properties dependancy

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"658fa5b2867c86001f19d6f4",
+				"658fa5b2867c86001f19d6f7"
+			]
+		},
+		"658fa5b2867c86001f19d6f4": {
+			"id": "658fa5b2867c86001f19d6f4",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"658fa5b2867c86001f19d6f7": {
+			"id": "658fa5b2867c86001f19d6f7",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
